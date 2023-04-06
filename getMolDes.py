8b2a51db33f6f322e2b3bf289ed88b0d37c32d60
XC-NCTR <83054794+XC-NCTR@users.noreply.github.com>
2023-04-06 01:23:44 -0500

    Create getMolDes.py

---
+++ getMolDes.py
@@ -0,0 +1,19 @@
+import os
+import pandas as pd
+from mordred import Calculator, descriptors
+from rdkit import Chem
+
+if __name__ == '__main__':
+    path = r'./'  # please set your own path
+
+    calc = Calculator(descriptors, ignore_3D=False)
+
+    SDFs = os.listdir(os.path.join(path,'SDFs'))
+    mols_3d = [Chem.MolFromMolFile(os.path.join(path,'SDFs','{}'.format(sdf))) for sdf in SDFs]
+    result_3d = calc.map(mols_3d)
+    result_3d = list(result_3d)
+    data = [result_3d[i].fill_missing(0).asdict() for i in range(len(result_3d))]
+    df = pd.DataFrame(data, index=[SDFs[i].strip('.sdf') for i in range(len(SDFs))])
+
+    #df = calc.pandas(mols_3d)
+    df.to_csv(os.path.join(path,'MolDescriptors.tsv'), sep='\t')
